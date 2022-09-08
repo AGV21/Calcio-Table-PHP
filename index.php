619576f091a2168f4b99da905341009dc9fcc590
Alex <alexgv2122@gmail.com>
2022-09-08 14:42:06 -0400

    Started project. added a teams array using php and started on a table using html

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Serie A</title>
+    </head>
+    <body>
+        <section> 
+            <div class="Table">
+                <table>
+                    <tr>
+                        <th>Club</th>
+                        <th>GP</th>
+                        <th>PTS</th>
+                    </tr>
+                </tablr>
+            </div>
+        </section>
+
+        <section>
+            <?php
+                $teams = array(
+                    "Atalanta"
+                    "Bolonga"
+                    "Cremonese"
+                    "Empoli"
+                    "Fiorentina"
+                    "Hellas Verona"
+                    "Inter Milan"
+                    "Juventus"
+                    "Lazio"
+                    "Lecce"
+                    "AC Milan"
+                    "Monza"
+                    "Napoli"
+                    "Roma"
+                    "Salernitana"
+                    "Sampdoria"
+                    "Sassuolo"
+                    "Spezia"
+                    "Torino"
+                    "Udinese")
+            ?>
+        </section>
+    </body>
+</html>
