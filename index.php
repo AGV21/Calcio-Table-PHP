6254366177a38b4408221500d39098c688285b7e
Alex <alexgv2122@gmail.com>
2022-09-20 14:09:14 -0400

    created a footer in and id body as mainContainer in html. in php, i added point from the 6th game for empoli and roma. added a function that echos the name of the league

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <title>Serie A</title>
     </head>
-    <body>
+    <body id="mainContainer">
         <section> 
             <div class="Table">
                 <table>
@@ -21,7 +21,7 @@
                      "Atalanta" => array(3, 1, 3, 3, 3, 1),
                      "Bolonga" => array(0, 1, 0, 1, 1, 3),
                      "Cremonese" => array(0, 0, 0, 0, 1, 1),
-                     "Empoli" => array(0, 1, 1, 1, 1),
+                     "Empoli" => array(0, 1, 1, 1, 1, 0),
                      "Fiorentina" => array(3, 1, 1, 0, 1, 0),
                      "Inter Milan" => array(3, 3, 0, 3, 0, 3),
                      "Juventus" => array(3, 1, 1, 3, 1, 1),
@@ -30,7 +30,7 @@
                      "AC Milan" => array(3, 1, 3, 1, 3, 3),
                      "Monza" => array(0, 0, 0, 0, 0, 1),
                      "Napoli" => array(3, 3, 1, 1, 3, 3),
-                     "Roma" => array(3, 3, 1, 3, 0),
+                     "Roma" => array(3, 3, 1, 3, 0, 3),
                      "Salernitana" => array(0, 1, 3, 1, 1, 1),
                      "Sampdoria" => array(0, 1, 0, 1, 0, 0),
                      "Sassuolo" => array(0, 3, 1, 1 ,1, 0),
@@ -38,21 +38,23 @@
                      "Torino" => array(3, 1, 3, 0, 3, 0),
                      "Udinese" => array(0, 1, 3, 3, 3, 3),
                      "Verona" => array(0, 1, 0, 1, 3, 0));
-                    
-                
+                    arsort($teams);
+                    function italy(){
+                        echo"Serie A <br>";
+                    }
+                    italy();
                     foreach ( $teams as $team => $points ) {
-                        echo( $team . " total points is ");
-                    
+                        //echo( $team . " total points is ");
                         $currentTeamPointsTotal = 0;
                         for( $currentTeamPointsIndex = 0; $currentTeamPointsIndex < count( $points ); $currentTeamPointsIndex++ ) {
-                            $currentTeamPointsTotal = $currentTeamPointsTotal + $points[ $currentTeamPointsIndex ];   
+                            $currentTeamPointsTotal = $currentTeamPointsTotal + $points[ $currentTeamPointsIndex ];
                         
                         }
-
-                        echo($currentTeamPointsTotal . "<br>" );
-    
-                    } 
-            ?>
+                    echo($currentTeamPointsTotal . "<br>" );}
+            ?>          
         </section>
     </body>
+    <footer> 
+        <P>AGV21 Project</p>
+    </footer> 
 </html>
